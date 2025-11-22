7ed1e576eec8677f0a69dab63a8d228d7e3b8fcd
kajatheepan <kajatheepan0311@gmail.com>
2025-11-22 14:17:43 +0530

    Initiate Telegram bot

---
+++ v1/bot.py
@@ -0,0 +1,19 @@
+import asyncio 
+
+from telethon import TelegramClient
+
+from core.config import settings
+from core.logging_setup import setup_logging
+
+
+client = TelegramClient("bot_session", settings.telegram_api_id, settings.telegram_api_hash)
+
+
+async def main():
+    setup_logging()
+    await client.start(bot_token=settings.telegram_bot_token)
+    await client.run_until_disconnected()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
